0430fc4d0a646e25c410bc0290e1a33bf0ccd9e8
Zac Hodge <zachodge7@gmail.com>
2024-04-24 16:34:47 +1000

    Removed commands requiring data storage or states

--- cogs/reactor.py
+++ cogs/reactor.py
@@ -1,8 +1,5 @@
-import os
-from typing import Final
-
 import discord
-from discord import Intents, Client, Message, Interaction, Reaction, app_commands
+from discord import Interaction, app_commands
 from discord.ext import commands
 from discord.ext.commands import Bot
 
@@ -29,52 +26,22 @@ class Reactor(commands.Cog):
             The top level discord.ext.commands.Bot object used for API interactions
         """
         self.bot: Bot = bot
-        # self.active: bool = False
-        self.channel: discord.interactions.InteractionChannel = None
         self.emojis: list[str] = []
-        self.history: dict = {"msgs": [], "emojis": []}
-
-    # SLASH COMMAND: REMOVES MOST RECENT REACTIONS
-    @app_commands.command()
-    async def react_undo(self, inter: Interaction) -> None:
-        """Removes the most recently applied emojis
-
-        Parameters
-        ----------
-        inter : Interaction
-            Interaction object given by the Discord API
-        """
-        if not self.history["msgs"] or not self.history["emojis"]:
-            await inter.response.send_message(
-                "No reactions to remove...", ephemeral=True
-            )
-            return
-
-        await inter.response.send_message(
-            "Removing latest reactions...", ephemeral=True
-        )
-        for i, msg in enumerate(self.history["msgs"]):
-            for emoji in self.history["emojis"][i]:
-                await msg.remove_reaction(emoji, self.bot.user)
-
-        await inter.edit_original_response(content="Removed latest reactions!")
 
     # SLASH COMMAND: REMOVES ALL BOT REACTIONS
     @app_commands.command()
-    async def react_remove(
-        self, inter: Interaction, number_of_messages: int = 10
-    ) -> None:
-        """Removes all of the bot's recent reactions in the current channel or thread (default: 10 messages)
+    async def remove(self, inter: Interaction, number_of_messages: int = 10) -> None:
+        """Removes all recent Rap's Reactor reactions in the current channel or thread (max: 100 messages, default: 10 messages)
 
         Parameters
         ----------
         inter : Interaction
             Interaction object given by the Discord API
         number_of_messages: int = 10 (optional)
-            How many messages to search through for Rap's Reactor reactions (max: 100)
+            How many messages to search through for this bot's reactions (max: 100)
         """
         await inter.response.send_message(
-            f"Removing reactions from the {min(number_of_messages, 100)} most recent messages...",
+            f"Removing this bot's reactions from the {min(number_of_messages, 100)} most recent messages...",
             ephemeral=True,
         )
 
@@ -90,42 +57,7 @@ class Reactor(commands.Cog):
 
     # SLASH COMMAND: REACT TO THREAD
     @app_commands.command()
-    async def react_now(self, inter: Interaction, emojis: str = None) -> None:
-        """Reacts to every message in this channel or thread with the given emojis until /react is used again with no arguments
-
-        Reacts with the given emojis to all messages in the current channel or thread until the command is called again or timeouts. Calling /react again updates the emojis used to react.
-
-        Parameters
-        ----------
-        inter : Interaction
-            Interaction object given by the Discord API
-        emojis : str, optional
-            The emojis to react with, if empty the bot is toggled off
-        """
-        if emojis:
-            self.channel = inter.channel
-            emoji_list, msg = await self.__extract_emoji(inter, emojis)
-            if emoji_list:
-                self.history: dict = {"msgs": [], "emojis": []}
-                self.emojis = emoji_list
-                await inter.response.send_message(
-                    f"Reacting with {' '.join(self.emojis)} {' '.join(msg)}",
-                    ephemeral=True,
-                )
-            else:
-                self.channel = None
-                await inter.response.send_message(
-                    f"You must provide at least one valid emoji", ephemeral=True
-                )
-        else:
-            self.channel = None
-            await inter.response.send_message(
-                f"Auto reacting has ended!", ephemeral=True
-            )
-
-    # SLASH COMMAND: REACT TO THREAD
-    @app_commands.command()
-    async def react_past(
+    async def react(
         self, inter: Interaction, emojis: str, number_of_messages: int = 32
     ) -> None:
         """Reacts to every message in this channel between your own reactions of :arrow_up: and :arrow_down: (pointing inwards)
@@ -135,7 +67,7 @@ class Reactor(commands.Cog):
         inter : Interaction
             Interaction object given by the Discord API
         emojis : str
-            The emojis to react with, if empty the bot is toggled off
+            The emojis to react with
         number_of_messages : int (optional)
             How many recent messages to search through (default: 32, max: 100)
         """
@@ -166,10 +98,6 @@ class Reactor(commands.Cog):
                                         f"Reacting with {emoji} in {msg.guild}.{msg.channel} : {msg.author}"
                                     )
                                     await msg.add_reaction(f"{emoji}")
-                            self.history = {
-                                "msgs": msgs,
-                                "emojis": [self.emojis] * len(msgs),
-                            }
                             return
                 await inter.edit_original_response(
                     content=f"Bounds not found. Please ensure at least one message is bounded by ⬆️ and ⬇️ reactions (pointing inwards)."
@@ -182,33 +110,6 @@ class Reactor(commands.Cog):
             await inter.response.send_message(
                 f"You must provide at least one valid emoji", ephemeral=True
             )
-
-    # LISTENER: ON MESSAGE REACT TO MESSAGE
-    @commands.Cog.listener("on_message")
-    async def react_now_reactor(self, msg: Message) -> None:
-        """Reacts to every message in this channel or thread until /react is used again with no arguments
-
-        Reacts with the given emojis to all messages in the current channel or thread until the command is called again or timeouts. Calling /react again updates the emojis used to react
-
-        Parameters
-        ----------
-        msg : Message
-            The Message object given by the Discord API
-        """
-        if (
-            msg.author.bot
-            or not self.channel
-            or not self.emojis
-            or msg.channel.id != self.channel.id
-        ):
-            return
-
-        for emoji in self.emojis:
-            print(f"Reacting with {emoji} in {msg.guild}.{msg.channel} : {msg.author}")
-            await msg.add_reaction(f"{emoji}")
-
-        self.history["msgs"].append(msg)
-        self.history["emojis"].append(self.emojis)
 
     async def __extract_emoji(self, inter: Interaction, emojis: str) -> list[str]:
         """Breaks a string emojis and custom emoji ids
